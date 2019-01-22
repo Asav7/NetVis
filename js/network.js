c3537cb935c2c0baba2c47ae81e88c34cb0afb99
Asav7 <albert.asav7@gmail.com>
2019-01-22 19:38:19 +0100

    add/edit modal functionality finished

--- js/network.js
+++ js/network.js
@@ -1,15 +1,14 @@
-
 let network;
 let nodes;
 let edges;
 
 // define some nodes for testing
 nodes = new vis.DataSet([
-    {id: 1, label: 'Node 1', x: 0, y: 0, shape: "dot", group: "dog", title: "etwas"},
+    {id: 1, label: 'Node 1', x: 0, y: 0, shape: "dot"},
     {id: 2, label: 'Node 2'},
     {id: 3, label: 'Node 3'},
     {id: 4, label: 'Node 4'},
-    {id: "doge", label: 'Node 5'}
+    {id: 5, label: 'Node 5'}
 ]);
 
 // define some edges for testing
@@ -17,7 +16,7 @@ edges = new vis.DataSet([
     {from: 1, to: 3},
     {from: 1, to: 2},
     {from: 2, to: 4},
-    {from: 2, to: "doge"},
+    {from: 2, to: 5},
 ]);
 
 // localize containter for network
@@ -41,96 +40,73 @@ let options = {
     physics: {
         enabled: false
     }
-
-
 };
 
-const addNodeModal = document.getElementById("new-node-modal");
+const nodeModal = document.getElementById("new-node-modal");
 
-
-// TODO: this has very limited functionality (edits only label); save button functionality is defined elsewere
 function editNode(data, callback) {
-    // TODO Change
-    addNodeModal.style.display = "block";
-
+    // Show modal with proper title
+    document.querySelector("#new-node-modal h3").innerText = "Edit node";
+    nodeModal.style.display = "block";
+    // Fill modal with data
+    document.getElementById("input-label").value = data.label;
+    document.getElementById("input-title").value = data.title ? data.title : "";
+    document.getElementById("input-group").value = data.group;
+    document.getElementById("input-nodeShape").value = data.shape;
+    // Add events
     document.getElementById("save-node-adding-button").onclick = saveEditedNode.bind(this, data, callback);
-    cancelAddNodeButton.onclick = cancelAddNode.bind(this, data, callback)
+    cancelNodeModal.onclick = cancelAddNode.bind(this, data, callback)
 }
 
 function saveEditedNode(data, callback) {
-
-    // get user's node shape
-    const newNodeShape = document.getElementById("add-node-shape").value; 
-    data.shape = newNodeShape;
-
-    // get user's node label
-    newNodeLabel = document.getElementById("input-label").value; 
-    data.label = newNodeLabel;
-
-    // get user's node group
-    data.group = document.getElementById("group-input").value;
-
-    // callback that creates node with changed data
+    // Sed nodes data to those specified by user
+    data.label = document.getElementById("input-label").value;
+    data.title = document.getElementById("input-title").value; 
+    data.group = document.getElementById("input-group").value;
+    data.shape = document.getElementById("input-nodeShape").value;
+    // Callback (edit node) and hide modal
     callback(data);
-    addNodeModal.style.display = "none";
-
-    
+    nodeModal.style.display = "none";
 }
-
-
-
 
 // Function for adding nodes (given to network.options.manipulation.addNode)
 function addNode(data, callback) {
-    console.log("inside")
-    
-    addNodeModal.style.display = "block";
-    document.getElementById("x-pos").value = data.x;
-    document.getElementById("y-pos").value = data.y;
-    document.getElementById("save-node-adding-button").onclick = saveAddNode.bind(this, data, callback);
+    // Show modal with proper title
+    document.querySelector("#new-node-modal h3").innerText = "Add node";
+    nodeModal.style.display = "block";
+    // Clear input fields
+    document.getElementById("input-label").value = "";
+    document.getElementById("input-title").value = "";
+    document.getElementById("input-group").value = "";
+    document.getElementById("input-nodeShape").value = "";
+    // Add events
+    document.getElementById("save-node-adding-button").onclick = saveAddedNode.bind(this, data, callback);
+    cancelNodeModal.onclick = cancelAddNode.bind(this, data, callback)
 }
 
-// TODO: this has to be bind to edit button functinality and add node functionality, because it does not restore edit node mode
-// toolbox to standard state
-// Cancel event for cancel button in add node popup 
-cancelAddNodeButton = document.getElementById("cancel-node-adding-button")
-cancelAddNodeButton.addEventListener("click", (e) => {e.preventDefault()})
+// Prevent refreshing for Save and Cancel buttons in nodeModal
+saveNodeModal = document.getElementById("save-node-adding-button")
+cancelNodeModal = document.getElementById("cancel-node-adding-button")
+saveNodeModal.addEventListener("click", (e) => {e.preventDefault()})
+cancelNodeModal.addEventListener("click", (e) => {e.preventDefault()})
+
+function saveAddedNode(data, callback) {
+    // Get user's data
+    data.label = document.getElementById("input-label").value;
+    data.title = document.getElementById("input-title").value; 
+    data.group = document.getElementById("input-group").value;
+    data.shape = document.getElementById("input-nodeShape").value;
+    // Callback (create node) and hide modal
+    callback(data);
+    nodeModal.style.display = "none";
+}
+
 function cancelAddNode(data, callback) {
     callback(null);
-    addNodeModal.style.display = "none";
+    nodeModal.style.display = "none";
 }
 
-
-// Save event for save button in add node popup
-saveAddNodeButton = document.getElementById("save-node-adding-button")
-saveAddNodeButton.addEventListener("click", (e) => {e.preventDefault()})
-
-function saveAddNode(data, callback) {
-
-    // get user's node shape
-    newNodeShape = document.getElementById("add-node-shape").value; 
-    data.shape = newNodeShape;
-
-    // get user's group
-    data.group = document.getElementById("group-input").value;
-
-    // callback that creates node with changed data
-    callback(data);
-    addNodeModal.style.display = "none";
-}
-
-
-
-
-
-
-
-
-
-// initialize your network!
-
+// Draw network
 function draw() {
     network = new vis.Network(container, data, options);
 }
-
-// draw()
