0e275a115a62fb39d5e13bb0a8c234430c92e71d
Asav7 <Albert.Asav7@gmail.com>
2019-01-31 12:25:58 +0100

    new dark style

--- js/network.js
+++ js/network.js
@@ -13,10 +13,10 @@
     // define some data for testing
     nodes = new vis.DataSet([
         {id: 1, label: 'Node 1', x: 0, y: 0, shape: "dot"},
-        {id: 2, label: 'Node 2'},
-        {id: 3, label: 'Node 3'},
-        {id: 4, label: 'Node 4'},
-        {id: 5, label: 'Node 5'}
+        {id: 2, label: 'Node 2', shape: "dot"},
+        {id: 3, label: 'Node 3', shape: "dot"},
+        {id: 4, label: 'Node 4', shape: "dot"},
+        {id: 5, label: 'Node 5', shape: "dot"}
     ]);
     edges = new vis.DataSet([
         {from: 1, to: 3},
@@ -27,11 +27,7 @@
 
     // Define variables necessary to create network
     let container = document.getElementById('mynetwork');
-    let data = {
-        nodes: nodes,
-        edges: edges
-    };
-    let options = {
+    let options_bck = {
         manipulation: {
             enabled: true,
             editNode: editNode,
@@ -40,6 +36,140 @@
         physics: {
             enabled: false
         }
+    };
+
+    let options = {
+        manipulation: {
+            enabled: true,
+            editNode: editNode,
+            addNode: addNode,
+            },
+        physics: {
+            enabled: false,
+            barnesHut: {
+                gravitationalConstant: -90000,
+                springConstant: 0.1,
+                springLength: 300,
+                centralGravity: 0.1,
+                avoidOverlap: 0.5,
+                },
+            stabilization: {
+                },
+            maxVelocity: 50,
+            minVelocity: 10000,
+            },
+        nodes: {
+            scaling: {
+                min: 5,
+                max: 150,
+                label: {
+                    enabled: true,
+                    max: 120,
+                    },
+                },
+            font: {
+                color: "#888888",
+                size: 20,
+                face: "Arial",
+                },
+			borderWidth: 2,
+            },
+        edges: {
+            width: 0.4,
+            inheritColor: "from",
+//			style: "dash",
+			selectionWidth: 5,
+			smooth: {
+			    enabled: true,
+			    roundness: 0.5,
+			    type: 'discrete',
+			    }
+
+			},
+        groups: {
+                13: {
+                    color: {
+                            border: "#2ae957",
+                            background: "#a2e5b2",
+                            highlight: {border: "#2ae957", background: "#a2e5b2"},
+                            hover: {border: "#2ae957", background: "#2ae957"},
+                    },
+                    font: {
+                        color: "rgba(255,255,255,1)",
+                        size: 12,
+                        face: "Tahoma",
+                    },
+                },
+                44: {
+                    borderWidth: 0.0,
+                    color: {
+                            border: "rgba(255,255,255,1)",
+                            background: "rgba(0,0,0,0)",
+//                            highlight: {border: "#2ae957", background: "#a2e5b2"},
+                            hover: {border: "#2ae957", background: "#2ae957"},
+                    },
+                    font: {
+                        color: "rgba(255,255,255,1)",
+                        size: 12,
+                        face: "Tahoma",
+                    },
+                },
+                dog: {
+                    color: {
+                            border: "yellow",
+                            background: "#a2e5b2",
+                            highlight: {border: "#2ae957", background: "#a2e5b2"},
+                            hover: {border: "#2ae957", background: "#2ae957"},
+                    },
+                    font: {
+                        color: "rgba(0,0,0,0.9)",
+                        size: 12,
+                        face: "Tahoma",
+                    },
+                },
+                3: {color: {
+                            border: "yellow",
+                            background: "#a2e5b2",
+                            highlight: {border: "#2ae957", background: "#a2e5b2"},
+                            hover: {border: "#2ae957", background: "#2ae957"},
+                        },
+                    font: {
+                        color: "#999999",
+                        size: 20,
+                        face: "Tahoma",
+                },
+                }, // blue
+                1: {color: {
+                        border: "#FFA500",
+                        background: "#FFFF00",
+                        highlight: { border: "#FFA500", background: "#FFFFA3" },
+                        hover: { border: "#FFA500", background: "#FFFFA3" }, // 1: yellow
+                        },
+                    font: {
+                        color: "#999999",
+                        size: 40,
+                        face: "Tahoma",
+                },
+                }, // blue
+                2: {color: {
+                        border: "#FA0A10",
+                        background: "#FB7E81",
+                        highlight: { border: "#FA0A10", background: "#FFAFB1" },
+                        hover: { border: "#FA0A10", background: "#FFAFB1" }
+                        },
+                    font: {
+                        color: "#999999",
+                        size: 20,
+                        face: "Tahoma",
+                },
+                }, // blue
+            },
+        interaction: {
+            hover: true,
+            tooltipDelay: 300,
+            navigationButtons: false,
+            hideEdgesOnDrag: true,
+            },
     };
 
     // Function for drawNetworking the network
@@ -61,6 +191,7 @@
     let titleIn = document.getElementById("input-title")
     let groupIn = document.getElementById("input-group")
     let shapeIn = document.getElementById("input-nodeShape")
+    let valueIn = document.getElementById("input-nodeSize")
 
     function editNode(data, callback) {
         // Show modal with proper title
@@ -71,6 +202,7 @@
         titleIn.value = data.title ? data.title : "";
         groupIn.value = data.group;
         shapeIn.value = data.shape;
+        valueIn.value = data.value;
         // Add events
         saveNodeModal.onclick = saveEditedNode.bind(this, data, callback);
         cancelNodeModal.onclick = cancelNodeAction.bind(this, data, callback)
@@ -81,6 +213,7 @@
             data.title = titleIn.value; 
             data.group = groupIn.value;
             data.shape = shapeIn.value;
+            data.value = valueIn.value;
             // Callback (edit node) and hide modal
             callback(data);
             nodeModal.style.display = "none";
@@ -97,6 +230,7 @@
         titleIn.value = "";
         groupIn.value = "";
         shapeIn.value = "";
+        valueIn.value = "";
         // Add events
         saveNodeModal.onclick = saveAddedNode.bind(this, data, callback);
         cancelNodeModal.onclick = cancelNodeAction.bind(this, data, callback)
@@ -107,6 +241,7 @@
             data.title = titleIn.value; 
             data.group = groupIn.value;
             data.shape = shapeIn.value;
+            data.value = valueIn.value;
             // Callback (create node) and hide modal
             callback(data);
             nodeModal.style.display = "none";
@@ -134,6 +269,7 @@
             node.label = netNode.label;
             node.title = netNode.title ? netNode.title : "";
             node.shape = netNode.shape;
+            node.value = netNode.value;
             node.group = netNode.group;
             node.x = network.getPositions(nodeID)[nodeID].x;
             node.y = network.getPositions(nodeID)[nodeID].y;
